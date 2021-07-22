92e512310711078dc98cd568cf2076a4bee04e7c
ayush-dedhia25 <ayushdedhia25@gmail.com>
2021-07-22 13:13:35 +0530

    Fixed userParams() Hook Error

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const ProductDetail : React.FC = () => {
-   const { id } : { id: string } = useParams();
+const ProductDetail: React.FC = () => {
+   const { id } = useParams<{id: number}>();
+   const productId: number = +id;
    
    return (
       <>
-         <h1>Product Details Page {id}</h1>
+         <h1>Product Details Page {productId}</h1>
       </>
    );
 }
